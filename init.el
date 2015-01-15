97c61c1eb83444299d7c38b20ca263b3847f3650
wu <wu@hostname.(none)>
2015-01-15 11:48:13 +0900

    switch to electric pair

--- init.el
+++ init.el
@@ -1,28 +1,31 @@
 (setq load-path (cons "~/.emacs.d/elisp" load-path))
 (require 'acp)
-(add-hook 'emacs-lisp-mode-hook 'acp-mode)
-(add-hook 'lisp-mode-hook 'acp-mode)
-(add-hook 'c++-mode-hook 'acp-mode)
-(add-hook 'tex-mode-hook 'acp-mode)
-(setq acp-paren-alist
-  '((?( . ?))
-    (?[ . ?])
-    (?{ . ?})
-    (?$ . ?$)
-    (?\" . ?\")
-    (?\' . ?\')))
-(setq acp-insertion-functions
-   '((mark-active . acp-surround-with-paren)
-     ((thing-at-point 'symbol) . acp-surround-symbol-with-paren)
-     (t . acp-insert-paren)))
-(defun acp-surround-symbol-with-paren (n)
-  (save-excursion
-    (save-restriction
-      (narrow-to-region (car (bounds-of-thing-at-point 'symbol)) (cdr (bounds-of-thing-at-point 'symbol)))
-      (goto-char (point-min))
-      (insert-char (car (acp-current-pair)) n)
-      (goto-char (point-max))
-      (insert-char (cdr (acp-current-pair)) n))))
+(add-hook 'c++-mode-hook 'electric-pair-mode)
+(add-hook 'tex-mode-hook 'electric-pair-mode)
+;; (add-hook 'emacs-lisp-mode-hook 'acp-mode)
+;; (add-hook 'lisp-mode-hook 'acp-mode)
+;; (add-hook 'c++-mode-hook 'acp-mode)
+;; (add-hook 'tex-mode-hook 'acp-mode)
+;; (setq acp-paren-alist
+;;   '((?( . ?))
+;;     (?[ . ?])
+;;     (?{ . ?})
+;;     (?$ . ?$)
+;;     (?\" . ?\")
+;;     (?\' . ?\')))
+;; (setq acp-insertion-functions
+;;    '((mark-active . acp-surround-with-paren)
+;;      ((thing-at-point 'symbol) . acp-surround-symbol-with-paren)
+;;      (t . acp-insert-paren)))
+;; (defun acp-surround-symbol-with-paren (n)
+;;   (save-excursion
+;;     (save-restriction
+;;       (narrow-to-region (car (bounds-of-thing-at-point 'symbol)) (cdr (bounds-of-thing-at-point 'symbol)))
+;;       (goto-char (point-min))
+;;       (insert-char (car (acp-current-pair)) n)
+;;       (goto-char (point-max))
+;;       (insert-char (cdr (acp-current-pair)) n)
+;; )))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'tabbar)
 (tabbar-mode)
@@ -64,6 +67,21 @@
 (global-set-key "\M-3" 'kill-buffer)
 ;off tool bar
 (tool-bar-mode -1)
+
+(defun my-tabbar-buffer-list ()
+  (delq nil
+        (mapcar #'(lambda (b)
+                    (cond
+                     ;; Always include the current buffer.
+                     ((eq (current-buffer) b) b)
+                     ((buffer-file-name b) b)
+                     ((char-equal ?\  (aref (buffer-name b) 0)) nil)
+		     ((equal "*scratch*" (buffer-name b)) b) ; *scratch*バッファは表示する
+		     ((char-equal ?* (aref (buffer-name b) 0)) nil) ; それ以外の * で始まるバッファは表示しない
+                     ((buffer-live-p b) b)))
+                (buffer-list))))
+(setq tabbar-buffer-list-function 'my-tabbar-buffer-list)
+
 ;;; 対応する括弧をハイライト
 (show-paren-mode t)
 ;; c coding style // c言語用
